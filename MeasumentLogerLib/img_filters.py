cff0cf3e4970dce76393084bba5a401745f23d0d
Gabriel-Almeida-ECAT <gabriel.olveira.ga76@gmail.com>
2023-12-18 22:44:09 -0400

    20231218#2
    abandon cv2 counter aproach and move MeasurementLogerLib

---
+++ MeasumentLogerLib/img_filters.py
@@ -0,0 +1,52 @@
+import cv2
+
+
+def gaussian_and_otsu(img_obj):
+    blur = cv2.GaussianBlur(img_obj, (7,7), 0)
+    ret, th = cv2.threshold(blur, 80, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    return th
+
+
+def equalize_hist(img_obj):
+    lab_img = cv2.cvtColor(img_obj, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab_img)
+    equa = cv2.equalizeHist(l)
+
+    equalized_lab_img = cv2.merge((equa, a, b))
+    equalized_img = cv2.cvtColor(equalized_lab_img, cv2.COLOR_LAB2BGR)
+
+    return equalized_img
+
+
+def CLAHE(img_obj):
+    lab_img = cv2.cvtColor(img_obj, cv2.COLOR_BGR2LAB)
+    l,a,b = cv2.split(lab_img)
+
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+    clahe_img = clahe.apply(l)
+
+    clahe_lab_img = cv2.merge((clahe_img, a, b))
+    clahe_img = cv2.cvtColor(clahe_lab_img, cv2.COLOR_LAB2BGR)
+
+    return clahe_img
+
+
+def get_gray_img(img_obj):
+    return cv2.cvtColor(img_obj, cv2.COLOR_BGR2GRAY)
+
+
+def adaptive_gaussian_threshold(img_obj):
+    blur = cv2.GaussianBlur(img_obj, (5, 5), 0)
+    return cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 55, 8)
+
+
+def get_contours(th):
+    rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    dilation = cv2.dilate(th, rect_kern, iterations=1)
+
+    try:
+        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    except:
+        ret_img, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    return sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pytesseract
 import PIL.Image
 import re
 
-from image_handler import img_filters
+from MeasumentLogerLib import img_filters
 
 
 cwd = os.getcwd()
@@ -38,12 +38,12 @@ def get_video_frames(video_path, test=False):
         #if counter == 0:
         if counter == video_frame_rate: #verificar apenas um dado por segundo
             equalized_img = img_filters.equalize_hist(frame)
-            #CLAHE_img = img_filters.CLAHE(frame)
             gray_img = img_filters.get_gray_img(equalized_img)
-            #gaussia_binary = img_filters.adaptive_threshold(gray_img)
+            gaussia_binary = img_filters.adaptive_gaussian_threshold(gray_img)
+            #otsu = img_filters.gaussian_and_otsu(gray_img)
 
             img_path = 'frames/' + str(img_number) + '.jpg'
-            cv2.imwrite(img_path, gray_img)
+            cv2.imwrite(img_path, gaussia_binary)
             #print(f'saved: {img_path}')
             img_number += 1
             counter = 0
@@ -58,67 +58,16 @@ def get_video_frames(video_path, test=False):
 def get_num_vals():
     num_vals = []
     for img in os.listdir(frames_path):
-        img_path = os.path.join('frames', img)
-        #img_path2 = os.path.join(frames_path, img) não funciona passar o full path pra imread por motivos de não sei
+        img_path = os.path.join('frames', img) #full path doesnt work
         img_obj = cv2.imread(img_path)
-        #gray_img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-        gray_img = img_filters.get_gray_img(img_obj)
-        #ret, th = img_filters.gaussian_and_otsu(gray_img)
-        th = img_filters.adaptive_threshold(gray_img)
 
-        cv2.imshow("aaa", th)
-        cv2.waitKey(0)
+        configs = '-c tessedit_char_whitelist=0123456789. --psm 7 --oem 3 digits'
 
-        contours = img_filters.get_contours(th)
-        #im2 = gray_img.copy()
-        im2 = img_obj.copy()
+        #text = pytesseract.image_to_string(PIL.Image.open(file), config=configs)
+        text = pytesseract.image_to_string(img_obj, config=configs)
+        #num_found = re.sub('[\d.]+', '', text)  # Regular Expression to match only digits and dot character => didin't work great
 
-        num = []
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            height, width, channels = im2.shape
-            # if height of box is not tall enough relative to total height then skip
-            #if (height / float(h)) > 6: continue
-
-            ratio = h / float(w)
-            # if height to width ratio is less than 1.5 skip
-            if ratio < 1.5: continue
-
-            # if width is not wide enough relative to total width then skip
-            if width / float(w) > 15: continue
-
-            #area = h * w
-            # if area is less than 100 pixels skip
-            #if area < 100: continue
-
-            # draw the rectangle
-            rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-            cv2.imshow("aaa", rect)
-            cv2.waitKey(0)
-
-            # grab character region of image
-            roi = th[y - 5:y + h + 5, x - 5:x + w + 5]
-            # perfrom bitwise not to flip image to black text on white background
-            roi = cv2.bitwise_not(roi)
-            # perform another blur on character region
-            roi = cv2.medianBlur(roi, 5)
-
-            cv2.imshow("aaa", roi)
-            cv2.waitKey(0)
-
-            configs = '-c tessedit_char_whitelist=0123456789. --psm 7 --oem 3 digits'
-            '''try:
-                text = pytesseract.image_to_string(roi, config=configs)
-                num_found = re.sub('[\d.]+', '', text) #Regular Expression to match only digits and dot
-            except:
-                num_found = None'''
-
-            num.append(pytesseract.image_to_string(roi, config=configs))
-            '''if num_found != None:
-                num.append(num_found)'''
-
-        num_vals.append(num)
+        num_vals.append(text)
 
     return num_vals
 
@@ -149,10 +98,6 @@ if __name__ == '__main__':
         get_video_frames('video_2023-11-01_12-08-43.mp4', test=True)
 
     numbers = get_num_vals()
-    '''for frame in list_frames:
-        file = 'frames/' + frame
-        text = pytesseract.image_to_string(PIL.Image.open(file), config=configs)
-        numbers.append(text)'''
 
     for ind in range(50):
         print(f'{ind}.png => \'{numbers[ind]}\'')
